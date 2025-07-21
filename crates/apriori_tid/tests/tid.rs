a906ccb13b7062fe017253777c26417a2561baef
Striker2783 <owenxingli@gmail.com>
2025-07-21 17:23:15 -0400

    TID Tests

---
+++ crates/apriori_tid/tests/tid.rs
@@ -0,0 +1,24 @@
+use std::collections::HashSet;
+
+use apriori::start::FrequentWriter;
+use apriori_tid::{hybrid::AprioriHybridRunner, tid::AprioriTIDRunner2};
+use tester::test_utils::{Solved, test_generic};
+
+#[test]
+fn test_tid() {
+    test_generic(|t, s| {
+        let tid = AprioriTIDRunner2::new(&t, s);
+        let mut writer: FrequentWriter<HashSet<Vec<usize>>> = FrequentWriter::new();
+        tid.run(&mut writer);
+        Solved::new(writer.into_inner())
+    });
+}
+#[test]
+fn test_hybrid() {
+    test_generic(|mut t, s| {
+        let tid = AprioriHybridRunner::new(&mut t, s);
+        let mut writer: FrequentWriter<HashSet<Vec<usize>>> = FrequentWriter::new();
+        tid.run(&mut writer);
+        Solved::new(writer.into_inner())
+    });
+}
