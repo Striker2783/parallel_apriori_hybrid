db85daea38c79b91726bf36c7f4fff022e253616
Striker2783 <owenxingli@gmail.com>
2025-07-21 17:19:56 -0400

    Apriori Tests

---
+++ crates/apriori/tests/apriori.rs
@@ -0,0 +1,25 @@
+use std::collections::HashSet;
+
+use apriori::{
+    alone::AprioriTrie, apriori::AprioriRunner, start::{Apriori, FrequentWriter}
+};
+use tester::test_utils::{Solved, test_generic};
+
+#[test]
+fn test_apriori() {
+    test_generic(|t, s| {
+        let a = AprioriRunner::new(&t, s);
+        let mut writer: FrequentWriter<HashSet<Vec<usize>>> = FrequentWriter::new();
+        a.run(&mut writer);
+        Solved::new(writer.into_inner())
+    });
+}
+#[test]
+fn test_apriori_trie() {
+    test_generic(|t, s| {
+        let a = AprioriTrie::new(t, s);
+        let mut writer: FrequentWriter<HashSet<Vec<usize>>> = FrequentWriter::new();
+        a.run(&mut writer);
+        Solved::new(writer.into_inner())
+    });
+}
