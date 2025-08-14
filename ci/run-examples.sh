af332c3d7d2ed2372b2defc05213359e128ecb3b
Striker2783 <38387609+Striker2783@users.noreply.github.com>
2025-08-14 18:05:23 -0400

    Create Parallelization Tests (#14)
    * Parallelization Test
    
    * Parallel Examples Workflow
    
    * Workflow Fix 1
    
    * .
    
    * Bash
    
    * Bash Added
    
    * Removed Print Statement

---
+++ ci/run-examples.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+examples=$(find examples -maxdepth 1 -type f -name '*.rs' -exec basename {} .rs \;)
+
+for example in $examples; do
+    echo "Running example: $example"
+    cargo mpirun --release --example $example
+    echo ""
+done
--- crates/tester/src/test_utils.rs
+++ crates/tester/src/test_utils.rs
@@ -6,8 +6,8 @@ use std::{
 };
 
 use apriori::transaction_set::TransactionSet;
-pub const SOLVED: &str = "../../test_files/solve1.dat";
-pub const DATABASE: &str = "../../test_files/test1.dat";
+pub const SOLVED: &str = "solve1.dat";
+pub const DATABASE: &str = "test1.dat";
 #[derive(Debug)]
 pub enum FromFileError {
     InvalidFormat(std::num::ParseIntError),
@@ -37,21 +37,37 @@ impl Solved {
         Ok(this)
     }
 }
+impl From<HashSet<Vec<usize>>> for Solved {
+    fn from(other: HashSet<Vec<usize>>) -> Self {
+        Self { set: other }
+    }
+}
 
-pub fn test_generic(f: impl Fn(TransactionSet, u64) -> Solved) {
-    let path = Path::new(DATABASE);
-    if !path.exists() {
+pub fn test_generic<T: AsRef<Path>>(test_files: T, f: impl Fn(TransactionSet, u64) -> Solved) {
+    test_generic_with_option(test_files, |t, s| Some(f(t, s)));
+}
+
+pub fn test_generic_with_option<T: AsRef<Path>>(
+    test_files: T,
+    f: impl Fn(TransactionSet, u64) -> Option<Solved>,
+) {
+    let database = test_files.as_ref().join(DATABASE);
+    let solved = test_files.as_ref().join(SOLVED);
+    if !database.exists() {
         panic!("Database file does not exist");
     }
-    let data = File::open(path).unwrap();
+    let data = File::open(database).unwrap();
     let t = TransactionSet::from_dat(data);
 
     let s = f(t, 10);
-    let path = Path::new(SOLVED);
-    if !path.exists() {
+    if !solved.exists() {
         panic!("Solved file does not exist");
     }
-    let f = File::open(SOLVED).expect("Solved File Not Found");
+    if s.is_none() {
+        return;
+    }
+    let s = s.unwrap();
+    let f = File::open(solved).expect("Solved File Not Found");
     let s2 = Solved::from_file(f).expect("Invalid Format Error");
     assert_eq!(s.set.len(), s2.set.len());
     assert_eq!(s, s2);
