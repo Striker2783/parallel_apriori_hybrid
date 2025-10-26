cacfab34d698ddcd4907cc237f512c7f720091e1
Striker2783 <38387609+Striker2783@users.noreply.github.com>
2025-10-26 12:23:26 -0400

    Fixed Bad I/O Disk Usage (#33)
    * Refactoring
    
    * Parition Iterator
    
    * Convertable
    
    * Count Distribution
    
    * Count Distribution Hybrid
    
    * Test Files Updated
    
    * New Profiling
    
    * Added a MPI flamegraph name

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ use count_distribution::runner::CountDistribution;
 use mpi::environment::{self, Universe};
 use mpi::traits::Communicator;
 use parallel::traits::ParallelRun;
+use pprof::ProfilerGuard;
 use std::fs::{File, OpenOptions};
 use std::io::{BufWriter, Write as IOWrite};
 use std::path::{Path, PathBuf};
@@ -28,6 +29,8 @@ pub struct Args {
     output: Option<PathBuf>,
     #[arg(long)]
     csv: Option<PathBuf>,
+    #[arg(long)]
+    profiler: Option<PathBuf>,
 }
 #[derive(Debug, Clone, ValueEnum)]
 pub enum Algorithms {
@@ -40,17 +43,19 @@ pub enum Algorithms {
 }
 
 pub struct Inputs<T: Write> {
-    data: TransactionSet,
+    data: PathBuf,
     support_count: u64,
     out: T,
+    guard: bool,
 }
 
 impl<T: Write> Inputs<T> {
-    pub fn new(data: TransactionSet, support_count: u64, out: T) -> Self {
+    pub fn new(data: PathBuf, support_count: u64, out: T, guard: bool) -> Self {
         Self {
             data,
             support_count,
             out,
+            guard,
         }
     }
 }
@@ -72,6 +77,15 @@ pub enum MainError {
 }
 
 static MPI_UNIVERSE: OnceLock<Universe> = OnceLock::new();
+static GUARD: OnceLock<ProfilerGuard> = OnceLock::new();
+
+pub fn get_guard() -> &'static ProfilerGuard<'static> {
+    GUARD.get_or_init(|| ProfilerGuard::new(100).unwrap())
+}
+
+pub fn guard_initialized() -> bool {
+    GUARD.get().is_some()
+}
 
 pub fn get_universe() -> &'static Universe {
     MPI_UNIVERSE.get_or_init(|| environment::initialize().expect("Failed to initialize MPI"))
@@ -81,39 +95,62 @@ pub fn mpi_initialized() -> bool {
     MPI_UNIVERSE.get().is_some()
 }
 
-fn aa<T: Write>(mut input: Inputs<T>, v: &Args) {
+fn aa<T: Write>(mut input: Inputs<T>, v: &Args) -> Result<(), std::io::Error> {
     match v.algorithm {
         Algorithms::Apriori => {
-            let runner = AprioriRunner::new(&input.data, input.support_count);
+            if input.guard {
+                get_guard();
+            }
+            let data = TransactionSet::from_path(&input.data)?;
+            let runner = AprioriRunner::new(&data, input.support_count);
             runner.run(&mut input.out);
         }
         Algorithms::CountDistribution => {
             let universe = get_universe();
+            if input.guard {
+                get_guard();
+            }
             let runner = CountDistribution::new(&input.data, input.support_count, &mut input.out);
             runner.run(universe);
         }
         Algorithms::AprioriTID => {
-            let runner = AprioriTIDRunner2::new(&input.data, input.support_count);
+            if input.guard {
+                get_guard();
+            }
+            let data = TransactionSet::from_path(&input.data)?;
+            let runner = AprioriTIDRunner2::new(&data, input.support_count);
             runner.run(&mut input.out);
         }
         Algorithms::AprioriHybrid => {
-            let runner = AprioriHybridRunner::new(&mut input.data, input.support_count);
+            if input.guard {
+                get_guard();
+            }
+            let mut data = TransactionSet::from_path(&input.data)?;
+            let runner = AprioriHybridRunner::new(&mut data, input.support_count);
             runner.run(&mut input.out);
         }
         Algorithms::CountDistributionHybrid => {
             let universe = get_universe();
+            if input.guard {
+                get_guard();
+            }
             let runner =
                 CountDistributionHybrid::new(&input.data, input.support_count, &mut input.out);
             runner.run(universe);
         }
         Algorithms::AprioriTrie => {
-            let runner = AprioriTrie::new(input.data, input.support_count);
+            if input.guard {
+                get_guard();
+            }
+            let runner =
+                AprioriTrie::new(TransactionSet::from_path(&input.data)?, input.support_count);
             runner.run(&mut input.out);
         }
     }
+    Ok(())
 }
 
-fn output_csv(file: &Path, percentage: f64, duration: &Duration) -> Result<(), MainError> {
+fn output_csv(file: &Path, duration: &Duration) -> Result<(), MainError> {
     let out = OpenOptions::new()
         .append(true)
         .create(true)
@@ -121,8 +158,6 @@ fn output_csv(file: &Path, percentage: f64, duration: &Duration) -> Result<(), M
         .map_err(MainError::InvalidOutputCSV)?;
     let mut writer = BufWriter::new(out);
     let mut s = String::new();
-    s.push_str(&percentage.to_string());
-    s.push(',');
     s.push_str(&duration.as_secs_f64().to_string());
     s.push('\n');
     let _ = IOWrite::write(&mut writer, s.as_bytes());
@@ -131,20 +166,27 @@ fn output_csv(file: &Path, percentage: f64, duration: &Duration) -> Result<(), M
 
 fn main() -> Result<(), MainError> {
     let a = Args::parse();
-    let file = File::open(&a.file).map_err(MainError::InvalidInputFile)?;
-    let data = TransactionSet::from_dat(file);
-    let size = data.len();
     let before = Instant::now();
     match &a.output {
         Some(f) => {
             let out = File::create(f).map_err(MainError::InvalidOutputFile)?;
             let writer = BufWriter::new(out);
-            let input = Inputs::new(data, a.support_count, writer);
-            aa(input, &a);
+            let input = Inputs::new(
+                a.file.clone(),
+                a.support_count,
+                writer,
+                a.profiler.is_some(),
+            );
+            aa(input, &a).unwrap();
         }
         None => {
-            let input = Inputs::new(data, a.support_count, EmptyWriter::new());
-            aa(input, &a);
+            let input = Inputs::new(
+                a.file.clone(),
+                a.support_count,
+                EmptyWriter::new(),
+                a.profiler.is_some(),
+            );
+            aa(input, &a).unwrap();
         }
     };
     if a.time {
@@ -152,13 +194,28 @@ fn main() -> Result<(), MainError> {
     }
     if let Some(p) = a.csv {
         if !mpi_initialized() || (mpi_initialized() && get_universe().world().rank() == 0) {
-            output_csv(
-                &p,
-                (a.support_count as f64) / (size as f64),
-                &before.elapsed(),
-            )?;
+            output_csv(&p, &before.elapsed())?;
         }
     }
-    unsafe { mpi::ffi::MPI_Finalize() };
+    if let Some(mut flamegraph) = a.profiler {
+        if guard_initialized() {
+            if let Ok(report) = get_guard().report().build() {
+                if mpi_initialized() {
+                    let mut stem = flamegraph.file_stem().unwrap().to_os_string();
+                    stem.push(get_universe().world().rank().to_string());
+                    stem.push(".");
+                    stem.push(flamegraph.extension().unwrap());
+                    flamegraph.set_file_name(stem);
+                }
+                let file = std::fs::File::create(&flamegraph).unwrap();
+                report.flamegraph(file).unwrap();
+            }
+        } else {
+            println!("Guard did not intialize");
+        }
+    }
+    if mpi_initialized() {
+        unsafe { mpi::ffi::MPI_Finalize() };
+    }
     Ok(())
 }
